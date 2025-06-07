3f787a784cfafc393de8a49022fdae7b9f3cb7f1
Ashley Lauren <ashley2.718282@gmail.com>
2025-06-07 11:30:07 -0500

    sqrt(x)

---
+++ binary-search/69-sqrt(x).py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, x
+        res = 0
+
+        while l <= r:
+            print(f"l=${l}, r=${r}")
+            m = (l + r) // 2
+            if m ** 2 == x:
+                return m
+            if m ** 2 > x:
+                r = m - 1
+            elif m ** 2 < x:
+                l = m + 1
+                res = m
+        return res
+        
